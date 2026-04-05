f693b65d1fbd8efbcb28ff5f2b05a0874ec3642b
Yan <yanlucasx10@gmail.com>
2026-04-05 18:25:09 -0300

    feat: configuracao do roteamento da página Sobre e Planos no app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Nav from './components/Nav'
 import Footer from './components/Footer'
 import Home from './routes/Home'
+import Sobre from "./routes/Sobre";
+import Planos from "./routes/Planos";
 
 function App() {
 
@@ -11,6 +13,8 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="/planos" element={<Planos />} />
       </Routes>
       <Footer />
     </BrowserRouter>
