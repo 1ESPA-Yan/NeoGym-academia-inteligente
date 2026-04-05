0995cbed18717b5cba2212ecd3cab6482a3a6d19
Gustavo <Gustavohiruo@gmail.com>
2026-04-05 01:21:37 -0300

    feat: configuracao do roteamento base no app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
