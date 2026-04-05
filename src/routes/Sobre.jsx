39e65144db93dbe894223de034199ba134da81b5
gustavo <gustavoczmacedo@gmail.com>
2026-04-05 15:24:30 -0300

    :feat: criacão da pagina sobre a academia

---
+++ src/routes/Sobre.jsx
@@ -0,0 +1,13 @@
+function Sobre() {
+  return (
+    <div>
+      <h1>Sobre a Academia</h1>
+      <p>
+        A Academia Inteligente tem como objetivo promover saúde e bem-estar
+        com tecnologia e qualidade.
+      </p>
+    </div>
+  );
+}
+
+export default Sobre;
